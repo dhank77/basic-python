f05eaf92990c1a8bddc9e4eb5f0043769e9784b1
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 10:50:16 +0800

    tugas 14

---
+++ 14-tugas.py
@@ -0,0 +1,21 @@
+# Exercise 14
+# Write a function definition named is_odd that takes in a number and returns True or False if that number is odd.
+import random
+    
+positive_even_number = random.randrange(2, 101, 2)
+negative_even_number = random.randrange(-100, -1, 2)
+
+positive_odd_number = random.randrange(1, 100, 2)
+negative_odd_number = random.randrange(-101, 0, 2)
+
+def is_odd(number) : 
+    if(number % 2) :
+        return True
+    else :
+        return False
+
+assert is_odd(positive_odd_number) == True, "Ensure that the function is defined, named properly, and returns the correct value"
+assert is_odd(positive_even_number) == False, "Ensure that the function is defined, named properly, and returns the correct value"
+assert is_odd(negative_odd_number) == True, "Ensure that the function is defined, named properly, and returns the correct value"
+assert is_odd(negative_even_number) == False, "Ensure that the function is defined, named properly, and returns the correct value"
+print("Exercise 14 is correct.")
