a2d4e354cf1ff0b29d25a9defbbcacc3d312b92f
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:33:22 +0800

    Tugas 2

---
+++ 2-tugas.py
@@ -0,0 +1,8 @@
+# Exercise 2
+# Create a variable named fruits and assign it a list of fruits containing the following fruit names as strings: 
+# mango, banana, guava, kiwi, and strawberry.
+
+fruits = ["mango", "banana", "guava", "kiwi", "strawberry"]
+
+assert fruits == ["mango", "banana", "guava", "kiwi", "strawberry"], "If you see an Assert Error, ensure the variable contains all the strings in the provided order"
+print("Exercise 2 is correct.")
