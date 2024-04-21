58607ff10ae9886d4ddbb2af428fff92f2a91e5c
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:32:05 +0800

    tugas 1

---
+++ 1-tugas.py
@@ -0,0 +1,6 @@
+# Exercise 1
+# On the line below, create a variable named on_mars_right_now and assign it the boolean value of False
+on_mars_right_now = False
+
+assert on_mars_right_now == False, "If you see a Name Error, be sure to create the variable and assign it a value."
+print("Exercise 1 is correct.")
