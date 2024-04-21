73bcbfba9c9f4d95926d4d67c2d4355f17d37ae6
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:17:27 +0800

    tugas 31

---
+++ 31-tugas.py
@@ -0,0 +1,15 @@
+# Exercise 31
+# Write a function definition named cube that takes in a number and returns the number times itself, times itself.
+import random
+    
+positive_even_number = random.randrange(2, 101, 2)
+positive_odd_number = random.randrange(1, 100, 2)
+
+def cube (number) :
+    return number * number * number
+
+assert cube(3) == 27
+assert cube(2) == 8
+assert cube(5) == 125
+assert cube(positive_odd_number) == positive_odd_number * positive_odd_number * positive_odd_number
+print("Exercise 31 is correct.")
