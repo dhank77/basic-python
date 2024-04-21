dadecea6d651af35eed53b2a07591b910e9951fb
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:40:01 +0800

    tugas 34

---
+++ 34-tugas.py
@@ -0,0 +1,10 @@
+# Exercise 34
+# Write a function definition named multiply that takes in two numbers and returns the first times the second argument.
+
+def multiply(number1, number2) :
+    return number1 * number2
+
+assert multiply(2, 1) == 2
+assert multiply(3, 5) == 15
+assert multiply(5, 2) == 10
+print("Exercise 34 is correct.")
