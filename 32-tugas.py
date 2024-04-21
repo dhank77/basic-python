536acd49068765bd7d36fcdf65fe677c2d77b383
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:27:34 +0800

    Create 32-tugas.py

---
+++ 32-tugas.py
@@ -0,0 +1,10 @@
+# Exercise 32
+# Write a function definition named square_root that takes in a number and returns the square root of the provided number
+import math
+def square_root(number) : 
+    return math.sqrt(number)
+
+assert square_root(4) == 2.0
+assert square_root(64) == 8.0
+assert square_root(81) == 9.0
+print("Exercise 32 is correct.")
