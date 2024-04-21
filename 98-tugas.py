e5196a7ad26a438a822ac9f7ce12977f39a342fd
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 22:29:57 +0800

    tugas 98

---
+++ 98-tugas.py
@@ -0,0 +1,44 @@
+shopping_cart = {
+    "tax": .08,
+    "items": [
+        {
+            "title": "orange juice",
+            "price": 3.99,
+            "quantity": 1
+        },
+        {
+            "title": "rice",
+            "price": 1.99,
+            "quantity": 3
+        },
+        {
+            "title": "beans",
+            "price": 0.99,
+            "quantity": 3
+        },
+        {
+            "title": "chili sauce",
+            "price": 2.99,
+            "quantity": 1
+        },
+        {
+            "title": "chocolate",
+            "price": 0.75,
+            "quantity": 9
+        }
+    ]
+}
+
+# Exercise 98
+# Write a function named total_number_of_items that takes in the shopping cart as input and returns the total number all item quantities.
+# This should return the sum of all of the quantities from each item type
+
+def total_number_of_items(data) :
+    count = 0
+    for x in data['items'] :
+        count += x['quantity']
+
+    return count
+
+assert total_number_of_items(shopping_cart) == 17
+print("Exercise 98 is complete.")
