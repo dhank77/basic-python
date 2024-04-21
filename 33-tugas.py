9b7b807fbdacd116e83883c513ba33fc90efb5db
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:38:45 +0800

    tugas 33

---
+++ 33-tugas.py
@@ -0,0 +1,10 @@
+# Exercise 33
+# Write a function definition named subtract that takes in two numbers and returns the first minus the second argument.
+
+def subtract(number1, number2) :
+    return number1 - number2
+
+assert subtract(8, 6) == 2
+assert subtract(27, 4) == 23
+assert subtract(12, 2) == 10
+print("Exercise 33 is correct.")
