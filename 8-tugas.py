febe0af0e0b9cbfc55cf5d0bfd28b019a8305369
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:42:22 +0800

    tugas 8

---
+++ 8-tugas.py
@@ -0,0 +1,8 @@
+# Exercise 8
+# Sort the vegetables in alphabetical order
+
+vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini", "tomato"]
+vegetables.sort()
+
+assert vegetables == ['broccoli', 'carrot', 'cauliflower', 'eggplant', 'tomato', 'zucchini']
+print("Exercise 8 is correct.")
