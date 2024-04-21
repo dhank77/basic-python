4b6532ab88660fcda1f7a1426f32d430f3392201
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 22:31:30 +0800

    tugas 99

---
+++ 99-tugas.py
@@ -0,0 +1,44 @@
+shopping_cart = {
+    "tax": .08,
+    "items": [
+        {
+            "title": "orange juice",
+            "price": 3.99,
+            "quantity": 1
+        },
+        {
+            "title": "rice",
+            "price": 1.99,
+            "quantity": 3
+        },
+        {
+            "title": "beans",
+            "price": 0.99,
+            "quantity": 3
+        },
+        {
+            "title": "chili sauce",
+            "price": 2.99,
+            "quantity": 1
+        },
+        {
+            "title": "chocolate",
+            "price": 0.75,
+            "quantity": 9
+        }
+    ]
+}
+
+# Exercise 99
+# Write a function named get_average_item_price that takes in the shopping cart as an input and returns the average of all the item prices.
+# Hint - This should determine the total price divided by the number of types of items. This does not account for each item type's quantity.
+
+def get_average_item_price(data) :
+    price = []
+    for x in data['items'] :
+        price.append(x['price'])
+
+    return sum(price) / len(price)
+
+assert get_average_item_price(shopping_cart) == 2.1420000000000003
+print("Exercise 99 is complete.")
