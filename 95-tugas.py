14ce07f8c3addb4c1120235280b0b761c8bdd82e
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 22:26:04 +0800

    tugas 95

---
+++ 95-tugas.py
@@ -0,0 +1,42 @@
+# Run this cell in order to have some setup data for the next exercises
+books = [
+    {
+        "title": "Genetic Algorithms and Machine Learning for Programmers",
+        "price": 36.99,
+        "author": "Frances Buontempo"
+    },
+    {
+        "title": "The Visual Display of Quantitative Information",
+        "price": 38.00,
+        "author": "Edward Tufte"
+    },
+    {
+        "title": "Practical Object-Oriented Design",
+        "author": "Sandi Metz",
+        "price": 30.47
+    },
+    {
+        "title": "Weapons of Math Destruction",
+        "author": "Cathy O'Neil",
+        "price": 17.44
+    }
+]
+# Exercise 95
+# Write a function called lowest_priced_book that takes in the above defined list of dictionaries "books" and returns the dictionary containing the title, price, and author of the book with the lowest priced book.
+# Hint: Much like sometimes start functions with a variable set to zero or float('inf'), you may want to create a dictionary with the price set to float('inf') to compare to each dictionary in the list
+
+def lowest_price_book(data) :
+    price = data[0]['price']
+    lowest_book = {}
+    for x in data :
+        if(price > x['price']) :
+            price = x['price']
+            lowest_book = x
+    return x
+
+assert lowest_price_book(books) == {
+    "title": "Weapons of Math Destruction",
+    "author": "Cathy O'Neil",
+    "price": 17.44
+}
+print("Exercise 95 is complete.")
