85ffc754dc9fe954eef8f3210a73ce68cb49854c
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:31:24 +0800

    solusi lain

--- 32-tugas.py
+++ 32-tugas.py
@@ -1,8 +1,12 @@
 # Exercise 32
 # Write a function definition named square_root that takes in a number and returns the square root of the provided number
-import math
+# import math
+# def square_root(number) : 
+#     return math.sqrt(number)
+
 def square_root(number) : 
-    return math.sqrt(number)
+    # return number ** (1/2)
+    return number ** 0.5
 
 assert square_root(4) == 2.0
 assert square_root(64) == 8.0
