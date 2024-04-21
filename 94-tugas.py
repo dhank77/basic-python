76177e49a07d986bf9acea98a6d3d307618f4837
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 22:23:00 +0800

    tugas 94

---
+++ 94-tugas.py
@@ -0,0 +1,43 @@
+# Run this cell in order to have some setup data for the next exercises
+books = [
+    {
+        "title": "Genetic Algorithms and Machine Learning for Programmers",
+        "price": 36.99,
+        "author": "Frances Buontempo"
+    },
+    {
+        "title": "The Visual Display of Quantitative Information",
+        "price": 38.00,
+        "author": "Edward Tufte"
+    },
+    {
+        "title": "Practical Object-Oriented Design",
+        "author": "Sandi Metz",
+        "price": 30.47
+    },
+    {
+        "title": "Weapons of Math Destruction",
+        "author": "Cathy O'Neil",
+        "price": 17.44
+    }
+]
+# Exercise 94
+# Write a function called highest_price_book that takes in the above defined list of dictionaries "books" and returns the dictionary containing the title, price, and author of the book with the highest priced book.
+# Hint: Much like sometimes start functions with a variable set to zero, you may want to create a dictionary with the price set to zero to compare to each dictionary's price in the list
+
+def highest_price_book(data) : 
+    price = 0
+    book = {}
+    for  x in data :
+        if(price < x['price']) :
+            price = x['price']
+            book = x
+    return book
+    
+assert highest_price_book(books) == {
+    "title": "The Visual Display of Quantitative Information",
+    "price": 38.00,
+    "author": "Edward Tufte"
+}
+
+print("Exercise 94 is complete")
