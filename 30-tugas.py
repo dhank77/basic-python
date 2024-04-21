3a511525c03a259cefcdeca6a8ba9163230e5dac
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:14:19 +0800

    tugas 30

---
+++ 30-tugas.py
@@ -0,0 +1,10 @@
+# Exercise 30
+# Write a function definition named add that takes in two numbers and returns the sum.
+
+def add(number1, number2):
+    return number1 + number2
+
+assert add(3, 2) == 5
+assert add(10, -2) == 8
+assert add(5, 7) == 12
+print("Exercise 30 is correct.")
