263ffa79066235b0c4d8cae2fd6b7bba834a9fe3
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:36:31 +0800

    tugas 4

---
+++ 4-tugas.py
@@ -0,0 +1,7 @@
+# Exercise 4
+# Create a variable named numbers and assign it a list of numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+assert numbers == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "Ensure the variable contains the numbers 1-10 in order."
+print("Exercise 4 is correct.")
