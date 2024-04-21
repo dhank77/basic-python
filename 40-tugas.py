4867a1d8944be877842957fe5d95be9dabc1d1cc
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:51:55 +0800

    tugas 40

---
+++ 40-tugas.py
@@ -0,0 +1,11 @@
+# Exercise 40
+# Write a function definition named area_of_rectangle that takes in two numbers and returns the product.
+
+def area_of_rectangle(number1, number2) :
+    return number1 * number2
+
+assert area_of_rectangle(1, 3) == 3
+assert area_of_rectangle(5, 2) == 10
+assert area_of_rectangle(2, 7) == 14
+assert area_of_rectangle(5.3, 10.3) == 54.59
+print("Exercise 40 is correct.")
