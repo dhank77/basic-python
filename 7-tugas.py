5f40c36a058ebd795fb7693d0097714f9cd90081
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:40:24 +0800

    tugas 7

---
+++ 7-tugas.py
@@ -0,0 +1,8 @@
+# Exercise 7
+# Given the list of numbers defined below, reverse the list of numbers that you created above. 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers.sort(reverse = True)
+
+
+assert numbers == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], "Assert Error means that the answer is incorrect." 
+print("Exercise 7 is correct.")
