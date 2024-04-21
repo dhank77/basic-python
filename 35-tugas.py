eff06e17f532f6ae9fc821d95d6d3711a36b0937
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:41:16 +0800

    tugas 35

---
+++ 35-tugas.py
@@ -0,0 +1,11 @@
+# Exercise 35
+# Write a function definition named divide that takes in two numbers and returns the first argument divided by the second argument.
+
+def divide(number1, number2) :
+    return number1 / number2
+
+assert divide(27, 9) == 3
+assert divide(15, 3) == 5
+assert divide(5, 2) == 2.5
+assert divide(10, 2) == 5
+print("Exercise 35 is correct.")
