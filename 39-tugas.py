fd9f0417f3975d823c82a0bba6e64cbc3a6731ef
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:47:44 +0800

    tugas 39

---
+++ 39-tugas.py
@@ -0,0 +1,12 @@
+# Exercise 39
+# Write a function definition named times_two_plus_three that takes in a number, multiplies it by two, adds 3 and returns the result.
+
+def times_two_plus_three(number) :
+    return number * 2 + 3
+
+assert times_two_plus_three(0) == 3
+assert times_two_plus_three(1) == 5
+assert times_two_plus_three(2) == 7
+assert times_two_plus_three(3) == 9
+assert times_two_plus_three(5) == 13
+print("Exercise 39 is correct.")
