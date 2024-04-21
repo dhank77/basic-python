4347c6dba99e053f8c1bf5bffd8e5635da5c2379
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:44:05 +0800

    tugas 9

---
+++ 9-tugas.py
@@ -0,0 +1,8 @@
+# Exercise 9
+# Write the code necessary to sort the fruits in reverse alphabetical order
+
+fruits = ["mango", "banana", "guava", "kiwi", "strawberry", "tomato"]
+fruits.sort(reverse = True)
+
+assert fruits == ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana']
+print("Exercise 9 is correct.")
