d1f5aff5bc1bb7b1ab61dbb13f05afac6a842efb
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 22:06:12 +0800

    tugas 91

---
+++ 91-tugas.py
@@ -0,0 +1,32 @@
+# Run this cell in order to have some setup data for the next exercises
+books = [
+    {
+        "title": "Genetic Algorithms and Machine Learning for Programmers",
+        "price": 36.99,
+        "author": "Frances Buontempo"
+    },
+    {
+        "title": "The Visual Display of Quantitative Information",
+        "price": 38.00,
+        "author": "Edward Tufte"
+    },
+    {
+        "title": "Practical Object-Oriented Design",
+        "author": "Sandi Metz",
+        "price": 30.47
+    },
+    {
+        "title": "Weapons of Math Destruction",
+        "author": "Cathy O'Neil",
+        "price": 17.44
+    }
+]
+
+# Exercise 91
+# Write a function named get_number_of_books that takes in a list of objects and returns the number of dictionaries in that list.
+
+def get_number_of_books(data) :
+    return len(data)
+
+assert get_number_of_books(books) == 4
+print("Exercise 91 is complete.")
