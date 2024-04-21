2ee63ea62895a8119763e31b6ca147aafeaac880
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 22:36:37 +0800

    tugas 100

---
+++ 100-tugas.py
@@ -0,0 +1,47 @@
+shopping_cart = {
+    "tax": .08,
+    "items": [
+        {
+            "title": "orange juice",
+            "price": 3.99,
+            "quantity": 1
+        },
+        {
+            "title": "rice",
+            "price": 1.99,
+            "quantity": 3
+        },
+        {
+            "title": "beans",
+            "price": 0.99,
+            "quantity": 3
+        },
+        {
+            "title": "chili sauce",
+            "price": 2.99,
+            "quantity": 1
+        },
+        {
+            "title": "chocolate",
+            "price": 0.75,
+            "quantity": 9
+        }
+    ]
+}
+
+# Exercise 100
+# Write a function named get_average_spent_per_item that takes in the shopping cart and returns the average of summing each item's quanties times that item's price.
+# Hint: You may need to set an initial total price and total total quantity to zero, then sum up and divide that total price by the total quantity
+
+def get_average_spent_per_item(data) :
+    total = 0
+    quantity = 0
+
+    for x in data['items'] :
+        total += x['price'] * x['quantity']
+        quantity += x['quantity']
+
+    return total / quantity
+    
+assert get_average_spent_per_item(shopping_cart) == 1.333529411764706
+print("Exercise 100 is complete.")
