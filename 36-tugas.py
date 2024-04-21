e27c2dc7f1cd146f28ba2ff6264f5ac4257bf473
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 13:42:51 +0800

    tugas 36

---
+++ 36-tugas.py
@@ -0,0 +1,10 @@
+# Exercise 36
+# Write a function definition named quotient that takes in two numbers and returns only the quotient from dividing the first argument by the second argument.
+
+def quotient(number1, number2) :
+    return number1 // number2
+
+assert quotient(27, 9) == 3
+assert quotient(5, 2) == 2
+assert quotient(10, 3) == 3
+print("Exercise 36 is correct.")
