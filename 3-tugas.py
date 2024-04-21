a361fea7cc5ecfce5abfb40ceefa33acc24d726e
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:34:35 +0800

    tugas 3

---
+++ 3-tugas.py
@@ -0,0 +1,8 @@
+# Exercise 3
+# Create a variable named vegetables and assign it a list of fruits containing the following vegetable names as strings: 
+# eggplant, broccoli, carrot, cauliflower, and zucchini
+
+vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"]
+
+assert vegetables == ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"], "Ensure the variable contains all the strings in the provided order"
+print("Exercise 3 is correct.")
