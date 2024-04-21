d49c39ae49a58a82ecf63ae6615dc067f8c64bd0
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:38:03 +0800

    tugas 5

---
+++ 5-tugas.py
@@ -0,0 +1,7 @@
+# Exercise 5
+# Given the following assigment of the list of fruits, add "tomato" to the end of the list. 
+fruits = ["mango", "banana", "guava", "kiwi", "strawberry"]
+fruits.append("tomato")
+
+assert fruits == ["mango", "banana", "guava", "kiwi", "strawberry", "tomato"], "Ensure the variable contains all the strings in the right order"
+print("Exercise 5 is correct")
