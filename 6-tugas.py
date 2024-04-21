e90aa816cbba646b8d82fe3c6dd8a30c24f9286e
Hamdani Ilham <d41113512@gmail.com>
2024-04-21 09:39:00 +0800

    tugas 6

---
+++ 6-tugas.py
@@ -0,0 +1,8 @@
+# Exercise 6
+# Given the following assignment of the vegetables list, add "tomato" to the end of the list.
+vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"]
+vegetables.append("tomato")
+
+
+assert vegetables == ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini", "tomato"], "Ensure the variable contains all the strings in the provided order"
+print("Exercise 6 is correct")
